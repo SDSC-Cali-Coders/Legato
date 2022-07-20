048ffdac0ca7c708f572a36ad52a127188204fb5
Akar <aks002@ucsd.edu>
2022-07-19 17:19:56 -0700

    Demo view attempted with carousel class
    Co-authored-by: calistajlee <calistajlee@users.noreply.github.com>
    Co-authored-by: lingyuchen <lingyuchen@users.noreply.github.com>

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from '../assets/icon.svg';
 import divider from '../assets/divider.svg';
+import pic1 from '../assets/listeninghistory.png';
+import pic2 from '../assets/concerts.png';
+import pic3 from '../assets/artistdescription.png';
+
+
 export default function Login() {
     return (
         <>
@@ -30,6 +35,32 @@ export default function Login() {
         - filling out carousel with demos of our app
         */}
             <section id="demo">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <div class="position-relative bottom-50 bottom-50">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-40 m-auto" src={pic1} alt="First slide (Listening History)"/>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-40 m-auto" src={pic2} alt="Second slide (Cocerts)"/>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-40 m-auto" src={pic3} alt="Third slide (Artist Description)"/>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+                        </a>
+                    </div>
+                </div> 
             </section>
         </>
     )
