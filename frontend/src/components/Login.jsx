36b77d96c9a72e6232a7282c47da70704186cb3d
Akar <aks002@ucsd.edu>
2022-07-19 17:18:31 -0700

    Built title bar
    Co-authored-by: lingyuchen <lingyuchen@users.noreply.github.com>

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import logo from '../assets/icon.svg';
+import divider from '../assets/divider.svg';
 export default function Login() {
     return (
         <>
             {/* Sectioning off page into 3 main sections */}
             <section id='title' className='p-2 bg-neutral-dark text-light'>
+                <div className='d-flex align-items-center'>
+                    <img src={logo} alt="" width="30" />
+                    <img src={divider} alt="" width="2" className='mx-3'/>
+                    <p className="Sofia_cursive fs-4 p-0">Legato</p>
+                </div>
             </section>
 
             <section id="blurb" className='p-4 bg-neutral-light'>
