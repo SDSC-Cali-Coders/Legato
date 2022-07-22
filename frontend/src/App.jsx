a6b2afef9d4718f68915a2b6196b9f9fd3bbb548
Akar <aks002@ucsd.edu>
2022-07-22 08:25:21 -0700

    Reorganized so each module has 33% of parent width

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,30 @@ function App() {
   return (
     <>
       <div className="container">
-        <row>
+        <div className="row">
           <div className="col">
+            TopThree Artists component here
             <TopThree.Artists></TopThree.Artists>
           </div>
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
+        <div className="row">
           <div className="col">
+            TopThree Tracks component here
             <TopThree.Tracks></TopThree.Tracks>
           </div>
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
+        <div className="row">
           <div className="col">
+            TopThree Genres component here
             <TopThree.Genres></TopThree.Genres>
           </div>
-        </row>
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
       </div>
     </>
   );
