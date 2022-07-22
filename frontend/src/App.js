995fc0c0ee99d02b9d0572cd65ee263730c96449
Akar <aks002@ucsd.edu>
2022-07-22 07:53:24 -0700

    Display all 3 module components for development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 import Login from './pages/Login';
+import TopThree from './components/listeningHistory/TopThree';
 
 function App() {
   return (
-    <Login/>
+    <>
+      <section>
+        <TopThree.Artists></TopThree.Artists>
+      </section>
+
+      <section>
+        <TopThree.Tracks></TopThree.Tracks>
+      </section>
+
+      <section>
+        <TopThree.Genres></TopThree.Genres>
+      </section>
+    </>
   );
 }
 
