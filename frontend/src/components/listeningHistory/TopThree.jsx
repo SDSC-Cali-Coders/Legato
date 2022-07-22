9aa16605f4a96dbf25330773f2dee540b2babf66
Akar <aks002@ucsd.edu>
2022-07-22 07:50:40 -0700

    Empty Module for TopThree

---
+++ frontend/src/components/listeningHistory/TopThree.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TopThree = {
+    Artists: (props) => {
+        return 
+        <>
+        </>
+    },
+    Tracks: (props) => {
+        return 
+        <>
+        </>
+    },
+    Genres: (props) => {
+        return 
+        <>
+        </>
+    }
+}
+
+export default TopThree;
