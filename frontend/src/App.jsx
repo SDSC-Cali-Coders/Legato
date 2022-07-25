7b96c3a7cdcffeaa08b6c0c0607bf55eaae25d38
JacobBolano <jacobcbolano@gmail.com>
2022-07-25 10:48:21 -0700

    added ability for button to go to authorization page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import ListeningHistory from './pages/ListeningHistory';
+import Login from './pages/Login'
 
 function App(props) {
   return (
     <>
       {props.isLoggedIn && <Navbar/>}
-      <ListeningHistory/>
+      <Login/>
     </>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App isLoggedIn={true}/>
+    <App isLoggedIn={false}/>
   </React.StrictMode>
 );
