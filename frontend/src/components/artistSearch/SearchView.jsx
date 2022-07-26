bffb5108096de2f378557f3776c7b6366e39c0af
Akar <aks002@ucsd.edu>
2022-07-25 19:40:03 -0700

    basic layout planned

--- frontend/src/components/artistSearch/SearchView.jsx
+++ frontend/src/components/artistSearch/SearchView.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+// Define an <ArtistResult/> component here
+// <div> - figure out how to align stuff :)
+// [img]..[Artist Name]....[Genre: genre]..........[subscribe + play btn group]
+const ArtistResult = (props) => {
+    return (
+        <>
+        </>
+    );
+}
 
 class SearchView extends Component {
     render() {
         return (
             <>
+            {/* Basic Layout
+                    <SearchBar.Long/> [placeholder for now]
+
+                    <div> aligned right w/ 2 selectable boxes [Subscribed/New]
+
+                    <ol> of <ArtistResult/> components
+             */}
             </>
         );
     }
