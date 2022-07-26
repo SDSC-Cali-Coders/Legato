cfc2c2ba0c411965a4279f3236c46419645dd024
LingYu Chen <lic015@ucsd.edu>
2022-07-26 11:39:33 -0700

    modified:   frontend/src/components/listeningHistory/UserCard.jsx

--- frontend/src/components/listeningHistory/UserCard.jsx
+++ frontend/src/components/listeningHistory/UserCard.jsx
@@ -5,7 +5,7 @@ const UserCard = (props) => {
     return (
         <div className="container Oswald_regular border rounded">
             <div className="row justify-content-start">
-                <div className="col-auto text-center">
+                <div className="col-5">
                     <img src={ThePolice} className="img rounded-circle" alt="..." width="50" />
                 </div>
                 <div className="col-auto text-center">
