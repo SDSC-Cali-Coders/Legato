aa88d68c353345e230c146c9ef83091e4d19c26e
JacobBolano <jacobcbolano@gmail.com>
2022-07-26 12:58:47 -0700

    Fixed merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from './components/Navbar';
 import ListeningHistory from './pages/ListeningHistory';
 import Login from './pages/Login'
 import { useState, useEffect } from 'react';
-import { accessToken, getTopSongs, getTopArtists } from './api/spotify';
+import { accessToken, getTopSongs, getTopArtists, getRecGenres } from './api/spotify';
 import { catchErrors, checkConcerts } from './utils';
 import { getArtistEvent } from './api/bandsintown';
+import genreIcon from './assets/genre-country.svg';
 
 let tracksObject;
 let artistsObject;
+let genresObject;
 const loggedIn = accessToken ? true : false;
 console.log("access token is" + accessToken);
 console.log("logged in variable is" + loggedIn);
@@ -17,6 +19,7 @@ function App(props) {
   const [token, setToken] = useState(null);
   const [topArtists, setTopArtists] = useState(null);
   const [topSongs, setTopSongs] = useState(null);
+  const [recGenres, setRecGenres] = useState(null);
 
   useEffect(() => {
     setToken(accessToken);
@@ -34,6 +37,14 @@ function App(props) {
     const fetchData = async () => {
       const { data } = await getTopSongs("short_term");
       setTopSongs(data);
+    };
+    catchErrors(fetchData());
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await getRecGenres();
+      setRecGenres(data);
     };
     catchErrors(fetchData());
   }, []);
@@ -95,6 +106,7 @@ function App(props) {
       }]
     }
   }
+
   if (topArtists != null) {
     artistsObject =
     {
@@ -155,6 +167,67 @@ function App(props) {
       }]
     }
   }
+  
+
+  if (recGenres != null) {
+    genresObject = {
+      selection: "Genres",
+      topThreeList: [{
+        rank: 1,
+        icon: genreIcon,
+        genre: recGenres.genres[0]
+      }, {
+        rank: 2,
+        icon: genreIcon,
+        genre: recGenres.genres[1]
+      }, {
+        rank: 3,
+        icon: genreIcon,
+        genre: recGenres.genres[2]
+      }],
+      topTenList: [{
+        icon: genreIcon,
+        genre: recGenres.genres[0],
+        percentage: 100
+      }, {
+        icon: genreIcon,
+        genre: recGenres.genres[1],
+        percentage: 90
+      }, {
+        icon: genreIcon,
+        genre: recGenres.genres[2],
+        percentage: 80
+      }, {
+        icon: genreIcon,
+        genre: recGenres.genres[3],
+        percentage: 70
+      }, {
+        icon: genreIcon,
+        genre: recGenres.genres[4],
+        percentage: 60
+      }, {
+        icon: genreIcon,
+        genre: recGenres.genres[5],
+        percentage: 50
+      }, {
+        icon: genreIcon,
+        genre: recGenres.genres[6],
+        percentage: 40
+      }, {
+        icon: genreIcon,
+        genre: recGenres.genres[7],
+        percentage: 30
+      }, {
+        icon: genreIcon,
+        genre: recGenres.genres[8],
+        percentage: 20
+      }, {
+        icon: genreIcon,
+        genre: recGenres.genres[9],
+        percentage: 10
+      }]
+    }
+  }
 
 
   return (
@@ -162,12 +235,12 @@ function App(props) {
       {loggedIn ? (
         <>
           <Navbar />
-          {topSongs && topArtists &&(
+          {topSongs && topArtists && recGenres && (
             topSongs != null ?
               <ListeningHistory
-                Tracks={tracksObject} Artists = {artistsObject}
+                Tracks={tracksObject} Artists = {artistsObject} Genres={genresObject}
               /> :
-              <ListeningHistory Tracks={false} Artists = {false}/>
+              <ListeningHistory Tracks={false} Artists = {false} Genres = {false}/>
           )}
         </>
       ) : (
--- frontend/src/pages/ListeningHistory.jsx
+++ frontend/src/pages/ListeningHistory.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import TopCard from '../components/listeningHistory/TopCard';
-//import artistImg from '../assets/ThePolice.jpg';
-import albumImg from '../assets/SimpleMindsAlbum.jpg';
-import genreIcon from '../assets/genre-country.svg';
 
 const ListeningHistory = (props) => {
     let TopCards = new Array(3);
 
-    TopCards[1] = <TopCard selection= 'Tracks'
-    topThreeList={props.Tracks.topThreeList} 
-    topTenList={props.Tracks.topTenList}/>
+    TopCards[1] = <TopCard selection='Tracks'
+        topThreeList={props.Tracks.topThreeList}
+        topTenList={props.Tracks.topTenList} />
     let selection = 'Artists';
-    TopCards[0] = <TopCard selection={selection} 
-    topThreeList={props.Artists.topThreeList} 
-    topTenList={props.Artists.topTenList}/>
+    TopCards[0] = <TopCard selection={selection}
+        topThreeList={props.Artists.topThreeList}
+        topTenList={props.Artists.topTenList} />
+    TopCards[2] = <TopCard selection='Genres'
+        topThreeList={props.Genres.topThreeList}
+        topTenList={props.Genres.topTenList} />
 
     // Can be 'turned on' once all branches are finished
     // ['Artists', 'Tracks', 'Genres'].forEach((selection, index) => {
@@ -22,62 +22,9 @@ const ListeningHistory = (props) => {
 
     return (
         <>
-        {/* Use props.Artists data instead of filler data */}
-        {TopCards[0]} 
-        {/* <TopCard selection="Artists" topThreeList={[
-            {rank: 1, img: artistImg, name: 'The Police', isSubscribed: false }, 
-            {rank: 2, img: artistImg, name: 'The Police', isSubscribed: false }, 
-            {rank: 3, img: artistImg, name: 'The Police', isSubscribed: false } 
-        ]} topTenList={[
-            {img: artistImg, name: 'The Police', isSubscribed: false },
-            {img: artistImg, name: 'The Police', isSubscribed: false },
-            {img: artistImg, name: 'The Police', isSubscribed: false },
-            {img: artistImg, name: 'The Police', isSubscribed: false },
-            {img: artistImg, name: 'The Police', isSubscribed: false },
-            {img: artistImg, name: 'The Police', isSubscribed: false },
-            {img: artistImg, name: 'The Police', isSubscribed: false },
-            {img: artistImg, name: 'The Police', isSubscribed: false },
-            {img: artistImg, name: 'The Police', isSubscribed: false },
-            {img: artistImg, name: 'The Police', isSubscribed: false }
-        ]}/>
-
-        {/* Use the props.Tracks data instead of filler data*/}
-        {TopCards[1]}
-        {/* <TopCard selection="Tracks" topThreeList={[
-            {rank: 1, img: albumImg},
-            {rank: 2, img: albumImg},
-            {rank: 3, img: albumImg}
-        ]} topTenList={[
-            {img: albumImg, name: 'Man in a Suitcase', artist: 'The Police'},
-            {img: albumImg, name: 'Man in a Suitcase', artist: 'The Police'},
-            {img: albumImg, name: 'Man in a Suitcase', artist: 'The Police'},
-            {img: albumImg, name: 'Man in a Suitcase', artist: 'The Police'},
-            {img: albumImg, name: 'Man in a Suitcase', artist: 'The Police'},
-            {img: albumImg, name: 'Man in a Suitcase', artist: 'The Police'},
-            {img: albumImg, name: 'Man in a Suitcase', artist: 'The Police'},
-            {img: albumImg, name: 'Man in a Suitcase', artist: 'The Police'},
-            {img: albumImg, name: 'Man in a Suitcase', artist: 'The Police'},
-            {img: albumImg, name: 'Man in a Suitcase', artist: 'The Police'}
-        ]}/> */}
-
-        {/* Use filler data until props.Genres data is available */}
-        {/* {TopCards[2]} */}
-        <TopCard selection="Genres" topThreeList={[
-            {rank: 1, icon: genreIcon, genre: 'Country'},
-            {rank: 2, icon: genreIcon, genre: 'Country'},
-            {rank: 3, icon: genreIcon, genre: 'Country'}
-        ]} topTenList={[
-            {icon: genreIcon, genre: 'Country', percentage: 100},
-            {icon: genreIcon, genre: 'Country', percentage: 90},
-            {icon: genreIcon, genre: 'Country', percentage: 85},
-            {icon: genreIcon, genre: 'Country', percentage: 75},
-            {icon: genreIcon, genre: 'Country', percentage: 72},
-            {icon: genreIcon, genre: 'Country', percentage: 60},
-            {icon: genreIcon, genre: 'Country', percentage: 53},
-            {icon: genreIcon, genre: 'Country', percentage: 49},
-            {icon: genreIcon, genre: 'Country', percentage: 29},
-            {icon: genreIcon, genre: 'Country', percentage: 13}
-        ]}/>
+            {TopCards[0]}
+            {TopCards[1]}
+            {TopCards[2]}
         </>
     );
 }
