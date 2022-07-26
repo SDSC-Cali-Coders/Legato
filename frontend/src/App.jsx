4737ea980b9d8791b066134411187770ee336d75
LingYu Chen <lic015@ucsd.edu>
2022-07-26 10:03:29 -0700

    modified:   frontend/src/App.jsx 	modified:   frontend/src/pages/Notification.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,18 @@ import Login from './pages/Login';
 import Navbar from './components/Navbar';
 import Topten from './components/listeningHistory/Topten';
 import Notification from './pages/Notification';
+import NotificationCard from './components/notification/NotificationCard';
+import genreIcon from './assets/genre-country.svg';
 
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Notification/>
+      <Navbar />
+      <NotificationCard.friendRequest name={"LingYu Chen"} message={"Hi"} img={genreIcon} />
+      <NotificationCard.artistsConcert artisitName={"Drake"} friendName={"Jacob Bolano"} img={genreIcon} />
+      <NotificationCard.friendConcert friendName={"LingYu Chen"} concertName={"One Ok Rock North America 2020"} img={genreIcon} />
+      <Notification name={"LingYu Chen"} favoriteArtists={"One Ok Rock, Survive Said the Prophet"} sharedArtists={"Drake"} img={genreIcon}/>
     </>
   );
 }
