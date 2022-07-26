1918a1ff1536c209dce34b3d7fda363492807d7a
LingYu Chen <lic015@ucsd.edu>
2022-07-26 11:36:42 -0700

    modified:   frontend/src/App.jsx 	new file:   frontend/src/components/listeningHistory/UserCard.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
+import UserCard from './components/listeningHistory/UserCard';
 import Navbar from './components/Navbar';
 import ListeningHistory from './pages/ListeningHistory';
+import icon from './assets/icon.svg';
 
 function App(props) {
   return (
     <>
       {props.isLoggedIn && <Navbar/>}
-      <ListeningHistory/>
+      <UserCard name={"Jane Doe"} mutualNumber={"5"} type={"Concerts"}/>
     </>
   );
 }
---
+++ frontend/src/components/listeningHistory/UserCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ThePolice from '../../assets/ThePolice.jpg';
+
+const UserCard = (props) => {
+    return (
+        <div className="container Oswald_regular border rounded">
+            <div className="row justify-content-start">
+                <div className="col-auto text-center">
+                    <img src={ThePolice} className="img rounded-circle" alt="..." width="50" />
+                </div>
+                <div className="col-auto text-center">
+                    <h5><b>{props.name}</b></h5>
+                    <h6>{props.mutualNumber} Mutual {props.type}</h6>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default UserCard;
