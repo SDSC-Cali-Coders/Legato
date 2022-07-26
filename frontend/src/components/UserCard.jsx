f2967e0ad4bc4af773fc3b367bf88bcce1b05c0b
Akar <aks002@ucsd.edu>
2022-07-26 14:46:30 -0700

    dynamic pfp icon

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ThePolice from '../../assets/ThePolice.jpg';
 
 const UserCard = (props) => {
     return (
         <div className="container Oswald_regular border rounded">
             <div className="row justify-content-start">
                 <div className="col-5">
-                    <img src={ThePolice} className="img rounded-circle" alt="..." width="50" />
+                    <img src={props.img} className="img rounded-circle" alt="..." width="50" />
                 </div>
                 <div className="col-auto text-center">
                     <h5><b>{props.name}</b></h5>
