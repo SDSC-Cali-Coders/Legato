9044a83e89d9a1c00f51b51d6e2f59208e664eae
Akar <aks002@ucsd.edu>
2022-07-26 14:46:51 -0700

    better display of UserCards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,39 @@
 import './App.css';
-import UserCard from './components/listeningHistory/UserCard';
+import UserCard from './components/UserCard';
 import Navbar from './components/Navbar';
 import ListeningHistory from './pages/ListeningHistory';
-import icon from './assets/icon.svg';
+import defProfileIcon from './assets/pfpIcon.svg';
 
 function App(props) {
   return (
     <>
       {props.isLoggedIn && <Navbar/>}
-      <UserCard name={"Jane Doe"} mutualNumber={"5"} type={"Concerts"}/>
+
+      <div className="container pt-5">
+        <div className="row">
+          <div className="col-3">
+            <UserCard img={defProfileIcon} name="Jane Doe" mutualNumber="5" type="Concerts"></UserCard>
+          </div>
+          <div className="col-3">
+            <UserCard img={defProfileIcon} name="Jane Doe" mutualNumber="5" type="Concerts"></UserCard>
+          </div>
+          <div className="col-3">
+            <UserCard img={defProfileIcon} name="Jane Doe" mutualNumber="5" type="Concerts"></UserCard>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-3">
+            <UserCard img={defProfileIcon} name="Jane Doe" mutualNumber="5" type="Friends"></UserCard>
+          </div>
+          <div className="col-3">
+            <UserCard img={defProfileIcon} name="Jane Doe" mutualNumber="5" type="Friends"></UserCard>
+          </div>
+          <div className="col-3">
+            <UserCard img={defProfileIcon} name="Jane Doe" mutualNumber="5" type="Friends"></UserCard>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
