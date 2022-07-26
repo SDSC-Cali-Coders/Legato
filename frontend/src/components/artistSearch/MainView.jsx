8e324c5b0bb3e89936c231d27987537920481f44
Akar <aks002@ucsd.edu>
2022-07-26 13:29:21 -0700

    documenting the struggle

--- frontend/src/components/artistSearch/MainView.jsx
+++ frontend/src/components/artistSearch/MainView.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const MainView = () => {
+const MainView = (props) => {
+    // Recipe for centering a div on a page:
+    // Container flexbox in the column orientation that takes entire height encapsulates
+    // row that grows to fill entire height w/ vertical center alignment for items
+    // col with w/ horizontal centering of text
     return (
         <div className='container'>
         </div>
