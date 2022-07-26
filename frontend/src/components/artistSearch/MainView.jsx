11d5ea1bad7dd68e55dbac820ab0003eebb25d61
Akar <aks002@ucsd.edu>
2022-07-26 13:29:55 -0700

    Implemented MainView w/ placeholder for searchbar

--- frontend/src/components/artistSearch/MainView.jsx
+++ frontend/src/components/artistSearch/MainView.jsx
@@ -6,7 +6,20 @@ const MainView = (props) => {
     // row that grows to fill entire height w/ vertical center alignment for items
     // col with w/ horizontal centering of text
     return (
-        <div className='container'>
+        <div className="container d-flex flex-column min-vh-100 Oswald_regular">
+            <div className="row flex-grow-1 align-items-center">
+                {/* Layout of MainView will be:
+                
+                    Searchbar.long [placeholder for now]
+
+                    Hint text for user
+                */}
+                <div className="col text-center mb-45">
+                    <span className="placeholder placeholder-lg w-100 mb-2"> Searchbar here </span>
+                    <p className="h3 fw-bold"> Search your subscribed Artists </p>
+                    <p className="h3 fw-bold"> and Explore new ones!</p>
+                </div>
+            </div>
         </div>
     );
 }
