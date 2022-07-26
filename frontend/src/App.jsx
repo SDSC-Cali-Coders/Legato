e304775edaa039a51dc504d46c85f8181c9656b1
Akar <aks002@ucsd.edu>
2022-07-25 19:30:16 -0700

    Empty component created and rendered for dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import ListeningHistory from './pages/ListeningHistory';
 
+// import ListeningHistory from './pages/ListeningHistory';
+// function App(props) {
+//   return (
+//     <>
+//       {props.isLoggedIn && <Navbar/>}
+//       <ListeningHistory/>
+//     </>
+//   );
+// }
+
+import SearchView from './components/artistSearch/SearchView';
 function App(props) {
   return (
     <>
       {props.isLoggedIn && <Navbar/>}
-      <ListeningHistory/>
+      <SearchView/>
     </>
   );
 }
-
 export default App;
---
+++ frontend/src/components/artistSearch/SearchView.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class SearchView extends Component {
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+
+SearchView.propTypes = {
+
+};
+
+
+export default SearchView;
