c66a57b6d79e21711c107d8b0bc016d4a16c92c7
Akar <aks002@ucsd.edu>
2022-07-27 15:24:23 -0700

    Invite friends btn & planned the rest of layout

--- frontend/src/components/settings/UserProfile.jsx
+++ frontend/src/components/settings/UserProfile.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Buttons from '../Buttons';
 
 const UserProfile = (props) => {
     return (
-        <div>
-            Placeholder for UserProfile
+        <div className="container mt-4 bg-warning">
+            {/* Row: Invite button */}
+            <div className="row">
+                <div className="col d-flex justify-content-end">
+                    <Buttons.Invite/>
+                </div>
+            </div>
+
+            {/* Row: Username - followers | following cnt */}
+            {/* Row: Socials icons w/ links */}
+            {/* Row: My Top Artists title */}
+            {/* Row: Dynamic amnt of ArtistCards [see more] button */}
+            {/* Row: [Top songs col] [Top genres col] */}
+            {/* Top songs col: title row; 5SongCard; [see more] button*/}
+            {/* Top genres col: title row; 5GenreCards; [see more] button*/}
+            {/* Row: [Log out] button */}
         </div>
     );
 }
