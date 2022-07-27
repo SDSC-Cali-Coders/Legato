e634f0a642f4ea02db60be8d147ac5f62d094e8b
Akar <aks002@ucsd.edu>
2022-07-27 08:19:58 -0700

    Empty component created and rendered for dev

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './scss/custom.scss';
 import App from './App';
+import Navbar from './components/Navbar';
+import ConfirmView from './components/settings/ConfirmView';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    {/* <App/> */}
+    <Navbar/>
+    <ConfirmView/>
   </React.StrictMode>
 );
