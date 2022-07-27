f2b518acb0c42a032e2ac7d2c0464bf4c453d36f
Akar <aks002@ucsd.edu>
2022-07-27 07:50:46 -0700

    profile visibility section added

--- frontend/src/components/settings/EditView.jsx
+++ frontend/src/components/settings/EditView.jsx
@@ -65,6 +65,32 @@ const EditView = (props) => {
 
                         {/* Row: Profile Visibility
                         +2 rows for each option */}
+                        <div className="row bg-light fs-3 mt-5">Profile Visibility</div>
+                        <div className="d-flex flex-row align-items-center mt-2 bg-danger">
+                            <i className="bi bi-eye-fill fs-4 bg-warning"></i>
+                            <div className="flex-grow-1 ps-3">
+                                <div className='fs-4'>Public</div>
+                                Everyone can see your profile information <br/>
+                                (profile picture, listening history, linked socials)
+                            </div>
+                            <div className="form-check form-check-reverse">
+                                <input type="radio" className="form-check-input" name='defaultVisibility' id='publicVisibilityOn'/>
+                                <label htmlFor="defaultVisibility" className="form-check-label">
+                                </label>
+                            </div>
+                        </div>
+                        <div className="d-flex flex-row align-items-center mt-2 bg-danger">
+                            <i className="bi bi-eye-slash-fill fs-4 bg-warning"></i>
+                            <div className="flex-grow-1 ps-3">
+                                <div className='fs-4'>Private</div>
+                                No one can see your profile information
+                            </div>
+                            <div className="form-check form-check-reverse">
+                                <input type="radio" className="form-check-input" name='defaultVisibility' id='publicVisibilityOff'/>
+                                <label htmlFor="defaultVisibility" className="form-check-label">
+                                </label>
+                            </div>
+                        </div>
 
                         {/* Row: Delete Acc button */}
                     </div>
