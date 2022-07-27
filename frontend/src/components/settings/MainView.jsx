c90268506642222304dad9b0fdeb0d179ce70ea6
Akar <aks002@ucsd.edu>
2022-07-26 17:53:06 -0700

    Layout planned and implemented w/ guiding colors

--- frontend/src/components/settings/MainView.jsx
+++ frontend/src/components/settings/MainView.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 const MainView = (props) => {
     return (
-        <div>
+        // Layout Will be a container w/
+        // empty column for centering main/central column
+        // Main centerd column holding the UserProfile component [placeholder div]
+        // column holding the settings icon up top
+        <div className="container py-5">
+            <div className="row text-center">
+                <div className="col-1 bg-danger"> </div>
+                <div className="col-10 bg-warning vh-100 fs-1"> UserProfileComponent Placeholder </div>
+                <div className="col-1 bg-danger">
+                    <i className="bi bi-gear-fill text-primary fs-1"></i>
+                </div>
+            </div>
         </div>
     );
 }
