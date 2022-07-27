c64e2beabc3f9b4b0ca1b3ccde00fb2790db9820
Akar <aks002@ucsd.edu>
2022-07-27 12:26:15 -0700

    Revert index prior to merge

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,24 +4,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './scss/custom.scss';
 import App from './App';
-import Navbar from './components/Navbar';
-import ConfirmView from './components/settings/ConfirmView';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App/> */}
-    <Navbar/>
-    <div className='container mt-5'>
-      <div className='row'>
-        <div className='col-3'>
-        </div>
-        <div className='col-6'>
-          <ConfirmView/>
-        </div>
-        <div className='col-3'>
-        </div>
-      </div>
-    </div>
+    <App/>
   </React.StrictMode>
 );
