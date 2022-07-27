8aa16ebedfa26a12bffce69144aad0abab699e53
Akar <aks002@ucsd.edu>
2022-07-26 23:33:25 -0700

    Generic settings buttons component created

--- frontend/src/components/settings/EditView.jsx
+++ frontend/src/components/settings/EditView.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+const Button = (props) => {
+    return (
+        <button className="btn btn-secondary px-4" type='button'>
+            {props.text}
+        </button>
+    );
+}
+
 const EditView = (props) => {
     return (
         <div>
