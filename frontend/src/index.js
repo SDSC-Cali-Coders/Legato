cecc7f3cb7c58b596568da36a46759c8608cb0e1
Akar <aks002@ucsd.edu>
2022-07-27 12:30:09 -0700

    revert index for smoother merges

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './scss/custom.scss';
 import App from './App';
-import EditView from './components/settings/EditView';
-import Navbar from './components/Navbar';
-import pfp from './assets/ThePolice.jpg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App/> */}
-    <Navbar/>
-    <EditView img={pfp}/>
+    <App/>
   </React.StrictMode>
 );
