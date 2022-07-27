f591e31d807d15f6ce3b514f7b370f6eacc7c0e8
Akar <aks002@ucsd.edu>
2022-07-27 07:56:30 -0700

    Testing out design w/ navbar too

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,13 @@ import ReactDOM from 'react-dom/client';
 import './scss/custom.scss';
 import App from './App';
 import EditView from './components/settings/EditView';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App/> */}
+    <Navbar/>
     <EditView/>
   </React.StrictMode>
 );
