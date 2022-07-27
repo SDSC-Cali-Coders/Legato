00710622cc1404f6d56da59048a063b4270b9b50
Akar <aks002@ucsd.edu>
2022-07-27 12:22:01 -0700

    Get rid of dev backgrounds + finishing touches

--- frontend/src/components/settings/ConfirmView.jsx
+++ frontend/src/components/settings/ConfirmView.jsx
@@ -3,22 +3,22 @@ import Buttons from '../Buttons';
 
 const ConfirmView = () => {
     return (
-        <div className="container Oswald_regular border border-dark bg-primary">
-            <div className="row fs-3 fw-bold text-center m-4 bg-light">
+        <div className="container Oswald_regular border border-dark bg-secondary">
+            <div className="row fs-3 fw-bold text-center m-4">
                 <div className="col">
                     Are you sure you want to delete <br/>
                     your account?
                 </div>
             </div>
-            <div className="row m-4 bg-light">
+            <div className="row m-4">
                 <div className="col">
                     <span className="fs-4 fw-bold">Warning: </span> 
                     Doing so will permanently delete your data, including your friends, <br/>
                     subscribed concerts, and linked accounts. You cannot get it back.
                 </div>
             </div>
-            <div className="row m-4 bg-light">
-                <div className="col d-flex justify-content-around">
+            <div className="row m-4">
+                <div className="col d-flex mt-5 justify-content-around">
                     <Buttons.Green text='Yes'/>
                     <Buttons.Red text='No'/>
                 </div>
