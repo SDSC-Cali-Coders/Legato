e74f78dece7572df2788c581b8af81d84d088071
Akar <aks002@ucsd.edu>
2022-07-27 15:48:07 -0700

    Username, socials, followers/following section

--- frontend/src/components/settings/UserProfile.jsx
+++ frontend/src/components/settings/UserProfile.jsx
@@ -3,16 +3,41 @@ import Buttons from '../Buttons';
 
 const UserProfile = (props) => {
     return (
-        <div className="container mt-4 bg-warning">
+        <div className="container mt-4 d-grid gap-3">
             {/* Row: Invite button */}
-            <div className="row">
+            <div className="row bg-warning">
                 <div className="col d-flex justify-content-end">
                     <Buttons.Invite/>
                 </div>
             </div>
 
-            {/* Row: Username - followers | following cnt */}
-            {/* Row: Socials icons w/ links */}
+            {/* Row: [Username & Socials] [followers | following cnt] */}
+            <div className="row bg-warning">
+                <div className="col-4 bg-primary offset-2">
+                    <div className="vstack">
+                        John Doe
+                        {props.username}
+                        <div className="hstack justify-content-between">
+                            <i className="bi bi-facebook"/>
+                            <i className="bi bi-twitter"/>
+                            <i className="bi bi-instagram"/>
+                            <i className="bi bi-pinterest"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="col hstack ms-4 bg-light">
+                    <div className='text-center bg-light fs-3'>
+                        {props.followersCount} 238
+                        <p className="fs-5">Followers</p>
+                    </div>
+                    <div className="vr my-4 mx-3"></div>
+                    <div className='text-center bg-light fs-3'>
+                        {props.followingCount} 101
+                        <p className="fs-5">Following</p>
+                    </div>
+                </div>
+            </div>
+
             {/* Row: My Top Artists title */}
             {/* Row: Dynamic amnt of ArtistCards [see more] button */}
             {/* Row: [Top songs col] [Top genres col] */}
