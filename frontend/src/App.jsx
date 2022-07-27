5add985e25abb377620fe73b2bd137a26603435a
LingYu Chen <lic015@ucsd.edu>
2022-07-27 12:28:40 -0700

    modified:   frontend/src/App.css 	modified:   frontend/src/App.jsx 	new file:   frontend/src/components/FindFriends.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,53 +10,13 @@ import Navbar from './components/Navbar';
 import UserCard from './components/UserCard';
 import defProfileIcon from './assets/pfpIcon.svg';
 
+import FindFriends from './components/FindFriends';
+
 function App(props) {
   return (
     <>
       {props.isLoggedIn && <Navbar/>}
-
-      <div className="container py-5">
-        <div className="row justify-content-between">
-          <div className="col-3">
-            <UserCard img={defProfileIcon} name="Jane Doe" mutualNumber="5" type="Concerts"></UserCard>
-          </div>
-          <div className="col-3">
-            <UserCard img={defProfileIcon} name="Jane Doe" mutualNumber="5" type="Concerts"></UserCard>
-          </div>
-          <div className="col-3">
-            <UserCard img={defProfileIcon} name="Jane Doe" mutualNumber="5" type="Concerts"></UserCard>
-          </div>
-        </div>
-
-        <div className="row justify-content-between">
-          <div className="col-3">
-            <UserCard img={defProfileIcon} name="Jane Doe" mutualNumber="5" type="Friends"></UserCard>
-          </div>
-          <div className="col-3">
-            <UserCard img={defProfileIcon} name="Jane Doe" mutualNumber="5" type="Friends"></UserCard>
-          </div>
-          <div className="col-3">
-            <UserCard img={defProfileIcon} name="Jane Doe" mutualNumber="5" type="Friends"></UserCard>
-          </div>
-        </div>
-      {/* </div>
-      <div className="d-flex m-5 justify-content-between align-items-center">
-        <ConcertResults.ConcertResultsCard />{" "} */}
-      {/* </div>
-      <div className="m-5 justify-content-between align-items-center">
-        <Searchbar.ConcertSearchbar />
-        <br />
-        <Searchbar.ArtistSearchbar />
-        <br />
-        <Searchbar.FindFriendsSearchbar />
-        <br /> */}
-        {/* <DropdownMenu.Radius />
-        <br/>
-        <DropdownMenu.ConcertSortBy />
-        <br/>
-        <DropdownMenu.FindFriendsSortBy /> */}
-      </div>
-
+      <FindFriends/>
     </>
   );
 }
