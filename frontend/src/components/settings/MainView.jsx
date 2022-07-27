b0fa45aa4674fc4621f8e381fd5cd2739f331df8
Akar <aks002@ucsd.edu>
2022-07-26 17:41:38 -0700

    Created empty component and rendered in index.js for test/dev

---
+++ frontend/src/components/settings/MainView.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MainView = (props) => {
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default MainView;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './scss/custom.scss';
 import App from './App';
+import MainView from './components/settings/MainView';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    {/* <App/> */}
+    <Navbar/>
+    <MainView/>
   </React.StrictMode>
 );
