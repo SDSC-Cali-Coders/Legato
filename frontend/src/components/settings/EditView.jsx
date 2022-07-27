b21e36955a37630db13455ee66f59980ddfeba2d
Akar <aks002@ucsd.edu>
2022-07-26 23:34:54 -0700

    Linked accounts section added

--- frontend/src/components/settings/EditView.jsx
+++ frontend/src/components/settings/EditView.jsx
@@ -33,7 +33,35 @@ const EditView = (props) => {
 
                         {/* Row: Linked Accounts
                         +4 rows for each SM acc */}
-        <div>
+                        <div className="row bg-light mt-5"> Linked accounts </div>
+                        <div className="row my-2 bg-primary">
+                            <div className="input-group align-items-center">
+                                <i className="bi bi-facebook fs-3"></i>
+                                <input type="text" className="form-control mx-4 bg-light" placeholder='https://facebook.com/JohnDoe'/>
+                                <i className="bi bi-x"></i>
+                            </div>
+                        </div>
+                        <div className="row my-2 bg-primary">
+                            <div className="input-group align-items-center">
+                                <i className="bi bi-twitter fs-3"></i>
+                                <input type="text" className="form-control mx-4 bg-light" placeholder='https://twitter.com/JohnDoe'/>
+                                <i className="bi bi-x"></i>
+                            </div>
+                        </div>
+                        <div className="row my-2 bg-primary">
+                            <div className="input-group align-items-center">
+                                <i className="bi bi-instagram fs-3"></i>
+                                <input type="text" className="form-control mx-4 bg-light" placeholder='https://instagram.com/JohnDoe'/>
+                                <i className="bi bi-x"></i>
+                            </div>
+                        </div>
+                        <div className="row my-2 bg-primary">
+                            <div className="input-group align-items-center">
+                                <i className="bi bi-pinterest fs-3"></i>
+                                <input type="text" className="form-control mx-4 bg-light" placeholder='https://pinterest.com/JohnDoe'/>
+                                <i className="bi bi-x"></i>
+                            </div>
+                        </div>
 
                         {/* Row: Profile Visibility
                         +2 rows for each option */}
