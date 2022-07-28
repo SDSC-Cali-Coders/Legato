343419bd9d97b725beb4dd4f860e4029e70c15eb
Akar <aks002@ucsd.edu>
2022-07-28 12:41:45 -0700

    Redesigned MainPage w/ grid system
    Co-authored-by: calistajlee <calistajlee@users.noreply.github.com>

--- frontend/src/components/concerts/MainConcert.jsx
+++ frontend/src/components/concerts/MainConcert.jsx
@@ -7,110 +7,62 @@ import BookmarkInterestedBtn from "./BookmarkInterestedBtn"
 
 export default function MainConcert() {
   return (
-    <>
+    <div className="container mt-3 min-vw-100 Oswald_regular">
 
       <Searchbar.ConcertSearchbar />
-      <div class="btn-group m-2" role="group" aria-label="First group">
+      <div className="btn-group m-2" role="group" aria-label="First group">
         <BookmarkInterestedBtn.YourBookmarks isPressed={false} />
       </div>
-      <div class="btn-group m-2" role="group" aria-label="Second group">
+      <div className="btn-group m-2" role="group" aria-label="Second group">
         <BookmarkInterestedBtn.Going isPressed={false} />
       </div>
 
-      <div className="Oswald_bold">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm">
-              <div className="d-flex p-2 mt-2 bg-neutral-primary border border-dark">
-                <h1>Recommended for you</h1>
-              </div>
+      {/* Row: [Recc col] [Nearby col] */}
+      <div className="row">
+        
+        {/* Recc col:
+              Row: title
+              Row: Grid [3x2] */}
+        <div className="col border border-dark bg-primary mx-4">
+          <div className="row fs-2"> 
+            <div className="col">
+              Recommended for you 
             </div>
+          </div>
+          <div className="row row-cols-3 g-4">
+            <div className="col"> <ConcertSearchCardResults.Card/> </div>
+            <div className="col"> <ConcertSearchCardResults.Card/> </div>
+            <div className="col"> <ConcertSearchCardResults.Card/> </div>
+            <div className="col"> <ConcertSearchCardResults.Card/> </div>
+            <div className="col"> <ConcertSearchCardResults.Card/> </div>
+            <div className="col"> <ConcertSearchCardResults.Card/> </div>
+          </div>
+        </div>
 
-            <div className="col-sm">
-              <div className="d-flex p-2 mt-2 bg-neutral-primary border border-dark">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-sm">
-                      <h1>Nearby you</h1>
-                    </div>
-                    <div class="col-sm">
-                      <DropdownMenu.Radius />
-                    </div>
-                  </div>
-                </div>
+        {/* Nearby col:
+              Row: title [radius btn]
+              Row: Grid [3x2] */}
+        <div className="col border border-dark bg-primary mx-4">
+          <div className="row align-items-center"> 
+            <div className="col-10 fs-2"> Nearby You</div>
+            <div className="col-2">
+              <div className="row">
+                <DropdownMenu.Radius />
               </div>
             </div>
           </div>
-        </div>
-      </div>
 
-      <div className="Oswald_bold">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm">
-              <div className="d-flex p-2 bg-neutral-primary border border-dark">
-                <div class="container-fluid">
-                  <div class="row">
-                    <div class="col">
-                      <ConcertSearchCardResults.card />
-                    </div>
-                    <div class="col">
-                      <ConcertSearchCardResults.card />
-                    </div>
-                    <div class="col">
-                      <ConcertSearchCardResults.card />
-                    </div>
-                    <div class="w-100"></div>
-                    <div class="col">
-                      <ConcertSearchCardResults.card />
-                    </div>
-                    <div class="col">
-                      <ConcertSearchCardResults.card />
-                    </div>
-                    <div class="col">
-                      <ConcertSearchCardResults.card />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-sm">
-              <div className="d-flex p-2 bg-neutral-primary border border-dark">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-sm">
-                      <div class="container-fluid">
-                        <div class="row">
-                          <div class="col">
-                            <ConcertSearchCardResults.card />
-                          </div>
-                          <div class="col">
-                            <ConcertSearchCardResults.card />
-                          </div>
-                          <div class="col">
-                            <ConcertSearchCardResults.card />
-                          </div>
-                          <div class="w-100"></div>
-                          <div class="col">
-                            <ConcertSearchCardResults.card />
-                          </div>
-                          <div class="col">
-                            <ConcertSearchCardResults.card />
-                          </div>
-                          <div class="col">
-                            <ConcertSearchCardResults.card />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="row row-cols-3 g-4">
+            <div className="col"> <ConcertSearchCardResults.Card/> </div>
+            <div className="col"> <ConcertSearchCardResults.Card/> </div>
+            <div className="col"> <ConcertSearchCardResults.Card/> </div>
+            <div className="col"> <ConcertSearchCardResults.Card/> </div>
+            <div className="col"> <ConcertSearchCardResults.Card/> </div>
+            <div className="col"> <ConcertSearchCardResults.Card/> </div>
           </div>
         </div>
       </div>
-    </>
+      
+    </div>
   );
 }
