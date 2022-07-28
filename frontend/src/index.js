9f14c8b08923e7a67d9a00294704986ae59b72b8
Akar <aks002@ucsd.edu>
2022-07-27 18:16:21 -0700

    Revert index.js for painless merge

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,16 +4,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './scss/custom.scss';
 import App from './App';
-import Navbar from './components/Navbar';
-
-import pfpImg from './assets/ThePolice.jpg';
-import Settings from './pages/Settings';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App/> */}
-    <Navbar/>
-    <Settings img={pfpImg}/>
+    <App/>
   </React.StrictMode>
 );
