d2d0fa0603d21515f75f0b657f4c9b7ae3ed03db
LingYu Chen <lic015@ucsd.edu>
2022-07-28 10:44:35 -0700

    modified:   frontend/src/components/concerts/ConcertsEmpty.jsx

--- frontend/src/components/concerts/ConcertsEmpty.jsx
+++ frontend/src/components/concerts/ConcertsEmpty.jsx
@@ -1,4 +1,3 @@
-<<<<<<< frontend/src/components/concerts/ConcertsEmpty.jsx
 import React from 'react';
 import Searchbar from '../Searchbar';
 
@@ -22,26 +21,4 @@ const ConcertsEmpty = (props) => {
     );
 }
 
-export default NoConcert;
-=======
-import React from "react";
-import ConcertPlaceholder from "./ConcertPlaceholder";
-import DropdownMenu from "../DropdownMenu";
-import ConcertSearchCardResults from "./ConcertSearchCardResults";
-
-export default function ConcertsEmpty() {
-  return (
-    <>
-      <ConcertPlaceholder />
-
-      <div className="Oswald_bold text-center">
-      <img src="https://pbs.twimg.com/media/FDkLGDmVUAAi0Ws.jpg" className="img-fluid text-center w-75" alt="Searching image"/>
-        <br/>
-        <div class="d-inline-flex p-5 m-2 bg-neutral-primary border border-dark">
-          You have no concerts you are currently planning on attending. Search for some concerts to add!
-        </div>
-      </div>
-    </>
-  );
-}
->>>>>>> frontend/src/components/concerts/ConcertsEmpty.jsx
+export default ConcertsEmpty;
