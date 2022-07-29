96cb8bf5965ac81cd3b7ba404b0235728ba846ba
LingYu Chen <lic015@ucsd.edu>
2022-07-29 14:47:16 -0700

    modified:   frontend/src/components/concerts/PrivateProfile.jsx     added bottom margin to the button.

--- frontend/src/components/concerts/PrivateProfile.jsx
+++ frontend/src/components/concerts/PrivateProfile.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import profile from '../assets/profile.svg';
-import darkdivider from '../assets/darkdivider.svg';
-import Buttons from './Buttons';
+import profile from '../../assets/profile.svg';
+import darkdivider from '../../assets/darkdivider.svg';
+import Buttons from '../Buttons';
 
 const PrivateProfile = (props) => {
     return (
         <div className="container position-relative m-5 p-5 bg-neutral-primary rounded Oswald_regular">
-            <div class="position-absolute top-0 start-0 rounded-circle translate-middle">
+            <div className="position-absolute top-0 start-0 rounded-circle translate-middle">
                 <img src={profile} className="border rounded-circle mx-3 bg-neutral-light" width="90" height="90"></img>
             </div>
             <div className="row-auto d-flex justify-content-sm-between pb-5">
@@ -32,7 +32,7 @@ const PrivateProfile = (props) => {
             <div className="row-auto d-flex justify-content-center">
                 <h5>Follow to see their account.</h5>
             </div>
-            <div className="row-auto d-flex justify-content-center">
+            <div className="row-auto d-flex justify-content-center mb-5">
                 <Buttons.Follow></Buttons.Follow>
             </div>
         </div>
