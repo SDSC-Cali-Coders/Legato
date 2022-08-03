4707a5c513043bf75d0daaadc989bd8d6b6305ce
calistajlee <calista.j.lee@gmail.com>
2022-08-02 21:18:35 -0700

    Initial commit for friends/other users interested in attending the same concert

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './scss/custom.scss';
-import App from './App';
-import EventInformation from './components/concerts/EventInformation';
-import Navbar from './components/Navbar';
-import Concerts from './pages/Concerts';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./scss/custom.scss";
+import App from "./App";
+import EventInformation from "./components/concerts/EventInformation";
+import Navbar from "./components/Navbar";
+import Concerts from "./pages/Concerts";
+import InterestedAttendees from "./components/concerts/InterestedAttendees";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App/> */}
-    <Navbar/>
-    {/* <Concerts/> */}
-    <EventInformation/>
+    <Navbar />
+    <InterestedAttendees mutualFriends="6" otherUsers="438" />
+    {/* <EventInformation/> */}
   </React.StrictMode>
 );
