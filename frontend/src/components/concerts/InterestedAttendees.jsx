6abf46ff67f0d760f595bc1894738c40bc02c816
Akar <aks002@ucsd.edu>
2022-08-04 13:54:15 -0700

    Dynamically render data from props, not hard-code
    Co-authored-by: calistajlee <calistajlee@users.noreply.github.com>

--- frontend/src/components/concerts/InterestedAttendees.jsx
+++ frontend/src/components/concerts/InterestedAttendees.jsx
@@ -1,222 +1,44 @@
 import React from "react";
 import UserCard from "../UserCard";
-import defProfileIcon from "../../assets/pfpIcon.svg";
 
 const InterestedAttendees = (props) => {
+  // const newArray = array.map(item => {return //what you want to return for each corresponding item in the array})
+  const mutualFriends = props.mutualFriends.map(item => {
+    return <UserCard img={item.img} name={item.name} mutualNumber={item.mutualNumber} type={item.type}/>
+  });
+
+  const otherUsers = props.otherUsers.map(item => {
+    return <UserCard img={item.img} name={item.name} mutualNumber={item.mutualNumber} type={item.type}/>
+  });
+
   return (
-    <>
-      <div className="container mt-3 min-vw-100 Oswald_regular">
-        <div className="container">
-          <div className="row">
-            <div className="col border">
-              <div className="row d-flex justify-content-center">
-                <div className="col-10 fs-2 text-center p-4">
-                  {props.mutualFriends} Mutual Friends
-                </div>
-              </div>
-            </div>
-
-            <div className="col border">
-              <div className="row d-flex justify-content-center">
-                <div className="col-10 fs-2 text-center p-4">{props.otherUsers} Others</div>
-              </div>
-            </div>
-            <div className="w-100"></div>
-
-            <div className="vertical-scroll col border p-1">
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-            </div>
-            <div className="vertical-scroll col border p-1">
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
-              <UserCard
-                img={defProfileIcon}
-                name="John Doe"
-                mutualNumber="5"
-                type="Concerts"
-              ></UserCard>
+    <div className="container mt-3 Oswald_regular">
+      <div className="row">
+        <div className="col border">
+          <div className="row d-flex justify-content-center">
+            <div className="col-10 fs-2 text-center p-4">
+              {mutualFriends.length} Mutual Friends
             </div>
           </div>
         </div>
+
+        <div className="col border">
+          <div className="row d-flex justify-content-center">
+            <div className="col-10 fs-2 text-center p-4">
+              {otherUsers.length} Others
+            </div>
+          </div>
+        </div>
+        <div className="w-100"></div>
+
+        <div className="vertical-scroll col border p-1">
+          {mutualFriends}
+        </div>
+        <div className="vertical-scroll col border p-1">
+          {otherUsers}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
