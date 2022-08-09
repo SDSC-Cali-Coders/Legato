5ba9dbb37faf0ed89e2fffdc6aa373409cd8aa38
Akar <aks002@ucsd.edu>
2022-08-08 18:29:37 -0700

    Public Profile demo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import Navbar from "./components/Navbar";
 import Concerts from "./pages/Concerts";
 import InterestedAttendees from "./components/concerts/InterestedAttendees";
 import defProfileIcon from "./assets/pfpIcon.svg";
+import PublicProfile from "./components/concerts/PublicProfile";
 
 const root  = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -27,11 +28,40 @@ let others = Array(438).fill({
   type: "Concerts",
 });
 
+let topArtists = Array(5).fill({
+  artistImg:defProfileIcon,
+  artistName:'John Doe'
+})
+
+let topSongs = Array(5).fill({
+  songTitle:'test',
+  artistName:'John Doe'
+})
+
+let topGenres = Array(5).fill({
+  genre:'rock'
+})
+
 root.render(
   <React.StrictMode>
     {/* <App/> */}
     <Navbar />
-    <InterestedAttendees mutualFriends={mutuals} otherUsers={others}/>
+    <PublicProfile
+      img = {defProfileIcon}
+      socialLinks={{
+        facebook: "https://facebook.com",
+        twitter: "https://twitter.com",
+        instagram: "https://instagram.com",
+        pinterest: "https://pinterest.com",
+      }}
+      userName='John Doe'
+      followingCount={87}
+      followersCount={101}
+      topArtistsList={topArtists}
+      topSongsList={topSongs}
+      topGenreList={topGenres}
+    />
+    {/* <InterestedAttendees mutualFriends={mutuals} otherUsers={others}/> */}
     {/* <EventInformation/> */}
   </React.StrictMode>
 );
