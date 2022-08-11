28181b2596dd1bd4257f5550eb127af310ad909b
Akar <aks002@ucsd.edu>
2022-08-10 18:41:02 -0700

    Dummy/Filler data created and passed into page

--- frontend/src/pageScripts/ArtistDescriptionScript.jsx
+++ frontend/src/pageScripts/ArtistDescriptionScript.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
 import ArtistDescription from '../components/artistSearch/ArtistDescription';
+import demoImg from '../assets/ThePolice.jpg'
+import defPfp from '../assets/profile.svg'
+
+const demoArtistData = {
+    img: demoImg,
+    name: 'The Police',
+    Genre: 'Rock',
+    Followers: 200102
+}
+
+const demoTopSongs = [
+    'Every Breath You Take',
+    'Roxanne',
+    'Message In A Bottle',
+    'Every Little Thing She Does Is Magic',
+    "Don't Stand So Close To Me",
+    'Walking On The Moon',
+    'So Lonely',
+    'De Do Do Do, De Da Da Da',
+    'King Of Pain',
+    'Wrapped Around Your Finger'
+]
+
+const userJaneDoe = {
+    pfp: defPfp,
+    userName: 'Jane Doe',
+    mutualNumber: 5,
+    type: 'Friends'
+}
+
+const demoUsersData = Array(demoArtistData.Followers).fill(userJaneDoe)
+
+const demoConcertData = Array(20).fill({
+    date: (new Date()),
+    venue: 'The Forum - Inglewood, CA'
+})
 
 const ArtistDescriptionScript = (props) => {
     return (
-        <ArtistDescription />
+        <ArtistDescription artist={demoArtistData} topSongs={demoTopSongs} users={demoUsersData} concerts={demoConcertData} />
     );
 }
 
