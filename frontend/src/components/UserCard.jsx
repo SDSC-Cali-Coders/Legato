e925470862f3a336c9948a95170d7abbd35d7cdb
Akar <aks002@ucsd.edu>
2022-08-10 18:41:30 -0700

    Brought in UserCard component from concerts branch

---
+++ frontend/src/components/UserCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const UserCard = (props) => {
+    return (
+        <div className="container Oswald_regular border rounded">
+            <div className="row justify-content-start">
+                <div className="col-5">
+                    <img src={props.img} className="img rounded-circle" alt="..." width="50" />
+                </div>
+                <div className="col-auto text-center">
+                    <h5><b>{props.name}</b></h5>
+                    <h6>{props.mutualNumber} Mutual {props.type}</h6>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default UserCard;
