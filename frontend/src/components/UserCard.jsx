01b133ac52ad000d58df7bfb07f4162d170b33be
calistajlee <calista.j.lee@gmail.com>
2022-08-15 23:54:19 -0700

    Center profile pic on all user cards

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -5,8 +5,9 @@ const UserCard = (props) => {
     return (
         <div className="container bg-primary pt-2 Oswald_regular border rounded">
             <div className="row justify-content-start">
-                <div className="col-5">
-                    <img src={props.img} className="img rounded-circle" alt="..." width="50" />
+                <div className="col-5 ">
+                    <img src={props.img} className="img-fluid rounded-circle d-flex m-auto" alt="profile pic" width="50" />
+
                 </div>
                 <div className="col-auto text-center">
                     <h5><b>{props.name}</b></h5>
