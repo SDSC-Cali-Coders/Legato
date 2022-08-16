dd0e1211951dc8f0afb349f494621eb1f426eef6
LinYuMachi <clyLINYU@hotmail.com>
2022-08-15 21:54:22 -0700

    modified:   frontend/src/components/notification/NotificationCard.jsx 	modified:   frontend/src/pages/NotificationView.jsx

--- frontend/src/pages/NotificationView.jsx
+++ frontend/src/pages/NotificationView.jsx
@@ -7,9 +7,14 @@ const NotificationView = (props) => {
         <>
             <div className="container align-items-center Oswald_regular">
                 {props.notifications.map(notification => (
-                    <NotificationCard.ArtistsEvent key={notification._id} img={"notification.img"} artistName={notification.associatedArtists[0]} 
-                    friendName={notification.associatedUsers[0]} onClick={() => props.onClick(notification._id)}
-                    />
+                    // <NotificationCard.ArtistsEvent key={notification._id} img={"notification.img"} 
+                    // artistName={notification.associatedArtists[0]} 
+                    // friendName={notification.associatedUsers[0]} onClick={() => props.onClick(notification._id)}
+                    // />
+                    <NotificationCard type={notification.type} key={notification._id} img={"notification.img"}
+                    artistName={notification.associatedArtists[0]} concertName={notification.associatedEvent}
+                    yourFriendName={notification.associatedUsers[0]}
+                    friendName={notification.associateUser} onClick={() => props.onClick(notification._id)}/>
                 ))}
             </div>
         </>
