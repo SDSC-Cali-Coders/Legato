213ef155c0adb3d5a2e0920ca16de4acdb150464
Akar <aks002@ucsd.edu>
2022-08-17 21:45:35 -0700

    Parse response data into searchResults

--- frontend/src/pageScripts/FriendsScript.jsx
+++ frontend/src/pageScripts/FriendsScript.jsx
@@ -8,19 +8,38 @@ const FriendsScript = () => {
   const [searchQuery, setSearchQuery] = useState('')
   const [searchResults, setSearchResults] = useState([])
 
-  // searchQuery updates triggers network call to backend
-  // response data is used to set searchResults
+  /** `searchQuery` updates triggers network call to backend
+   * `response.data` used to set `searchResults`
+  */
   useEffect(() => {
     if (!searchQuery) return setSearchResults([])
 
     axios.get(`http://localhost:27017/friends/${searchQuery.toLowerCase()}`)
+
+    // onSuccess, parse response for relavant data
     .then((response) => {
-      setSearchResults(response.data)
+      setSearchResults(
+        response.data.map(userData => {
+          return {
+            img: userData.img,
+            name: userData.name,
+            followers: userData.followers,
+            mutualFriends: 5,     // hard-coded for now due to 
+            mutualConcerts: 2,    // complexity of determing "mutual" data
+            mutualArtists: 8,
+            isFollowing: false    // also hard-coded for now due to complexity
+          }
+        }
+      ))
     })
+
+    // onFailure, log error for debugging
     .catch((error) => {
       console.log(error)
-    }).then(() => {
-      // Put any "final" logic here (i.e. stuff you always want to run)
+    })
+    
+    // "final" logic, i.e. any stuff you want to always run
+    .then(() => {
     }
     )
   },
