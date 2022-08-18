b42b52ec3af94a5c73d6828f9d4b0bf3c1176c05
Akar <aks002@ucsd.edu>
2022-08-17 21:45:49 -0700

    Render searchResults instead of dummy data

--- frontend/src/pageScripts/FriendsScript.jsx
+++ frontend/src/pageScripts/FriendsScript.jsx
@@ -45,24 +45,11 @@ const FriendsScript = () => {
   },
   [searchQuery])
 
-  let friendCard1 = Array(2).fill({
-    img: defProfileIcon,
-    name: "John Doe",
-    followers: "41",
-    mutualFriends: "5",
-    type1: "Friends",
-    mutualConcerts: "2",
-    type2: "Concerts",
-    mutualArtists: "8",
-    type3: "Artists",
-    isFriendAdded: false
-  });
-
   function handleSearch(query) {
     setSearchQuery(query)
   }
 
-  return <Friends card={friendCard1} handleSearch={handleSearch}/>
+  return <Friends card={searchResults} handleSearch={handleSearch}/>
 };
 
 export default FriendsScript;
