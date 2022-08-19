08b4b0f49f26bb7fe37af2220d0b022b5c6bb6bf
Akar <aks002@ucsd.edu>
2022-08-18 20:47:28 -0700

    added audio element - triggered by btn click

--- frontend/src/components/Buttons.jsx
+++ frontend/src/components/Buttons.jsx
@@ -1,83 +1,99 @@
-import React from 'react';
+import React from "react";
 
 const Buttons = {
-    Subscribe: () => {
-        return (
-        <button className="btn btn-success text-scaling fs-7" type="button" >
-            Subscribe
-            <i className="bi bi-plus-lg ps-sm-2"></i>
-        </button>);
-    },
-    Unsubscribe: () => {
-        return (
-        <button className="btn btn-danger" type="button">
-            Subscribed
-            <i className="bi bi-dash-lg ps-2"></i>
-        </button>
-        );
-    },
-    Play: (props) => {
-        return (
-            <i className="bi bi-caret-right-fill btn-play" type="button"></i>
-        );
-    },
-    SeeMore: () => {
-        return (
-        <button className="btn btn-align btn-secondary btn-sm border-dark rounded-pill" type='button'>
-            See More 
-            <i className="bi bi-chevron-right ps-2"></i>
-        </button>
-        );
-    },
-    Invite: () => {
-        return (
-            <button className="btn btn-align btn-secondary border-dark" type="button">
-                Invite Friends <i className="bi bi-link ps-2 bi-lg fs-4"></i>
-            </button>
-		);
-    },
-    Close: () => {
-        return (
-            <button className="btn btn-secondary btn-sm rounded-circle border-dark">
-                <i className="bi bi-x"></i>
-            </button>
-		);
-    },
-    Green: (props) => {
-        return (
-            <button className="btn btn-colored btn-success border-dark">
-                {props.text}
-            </button>
-		);
-    },
-    Red: (props) => {
-        return (
-            <button className="btn btn-colored btn-danger border-dark">
-                {props.text}
-            </button>
-		);
-    },
-    Followers: () => {
-        return (
-            <button className="btn btn-follow btn-secondary border-dark fw-semibold">
-                Followers
-            </button>
-		);
-    },
-    Following: () => {
-        return (
-            <button className="btn btn-follow btn-light border-dark text-light fw-semibold">
-                Following
-            </button>
-		);
-    },
-    Follow: () => {
-        return (
-            <button className="btn btn-follow-short bg-neutral-body py-0 px-4 border-dark text-light fw-semibold">
-                Follow
-            </button>
-		);
-    }
-}
+  Subscribe: () => {
+    return (
+      <button className="btn btn-success text-scaling fs-7" type="button">
+        Subscribe
+        <i className="bi bi-plus-lg ps-sm-2"></i>
+      </button>
+    );
+  },
+  Unsubscribe: () => {
+    return (
+      <button className="btn btn-danger" type="button">
+        Subscribed
+        <i className="bi bi-dash-lg ps-2"></i>
+      </button>
+    );
+  },
+  Play: (props) => {
+    return (
+      <>
+        <i
+          className="bi bi-caret-right-fill btn-play"
+          type="button"
+          onClick={(e) => {
+            const audioPlayer = e.target.nextElementSibling
+            audioPlayer.paused ? audioPlayer.play() : audioPlayer.pause()
+          }}
+        >
+          {" "}
+        </i>
+        <audio src={props.preview_url}></audio>
+      </>
+    );
+  },
+  SeeMore: () => {
+    return (
+      <button
+        className="btn btn-align btn-secondary btn-sm border-dark rounded-pill"
+        type="button"
+      >
+        See More
+        <i className="bi bi-chevron-right ps-2"></i>
+      </button>
+    );
+  },
+  Invite: () => {
+    return (
+      <button className="btn btn-align btn-secondary border-dark" type="button">
+        Invite Friends <i className="bi bi-link ps-2 bi-lg fs-4"></i>
+      </button>
+    );
+  },
+  Close: () => {
+    return (
+      <button className="btn btn-secondary btn-sm rounded-circle border-dark">
+        <i className="bi bi-x"></i>
+      </button>
+    );
+  },
+  Green: (props) => {
+    return (
+      <button className="btn btn-colored btn-success border-dark">
+        {props.text}
+      </button>
+    );
+  },
+  Red: (props) => {
+    return (
+      <button className="btn btn-colored btn-danger border-dark">
+        {props.text}
+      </button>
+    );
+  },
+  Followers: () => {
+    return (
+      <button className="btn btn-follow btn-secondary border-dark fw-semibold">
+        Followers
+      </button>
+    );
+  },
+  Following: () => {
+    return (
+      <button className="btn btn-follow btn-light border-dark text-light fw-semibold">
+        Following
+      </button>
+    );
+  },
+  Follow: () => {
+    return (
+      <button className="btn btn-follow-short bg-neutral-body py-0 px-4 border-dark text-light fw-semibold">
+        Follow
+      </button>
+    );
+  },
+};
 
 export default Buttons;
